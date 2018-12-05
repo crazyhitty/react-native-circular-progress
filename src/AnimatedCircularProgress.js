a9da532adfdc0b41b9016f1a2cc1c6fef35ee36c
Kartik Sharma <kartik.sharma@myntra.com>
2018-12-05 17:26:20 +0530

    Update AnimatedCircularProgress.js
    Provide onComplete callback the provides the actual animated value when animation completes.

--- src/AnimatedCircularProgress.js
+++ src/AnimatedCircularProgress.js
@@ -28,16 +28,32 @@ export default class AnimatedCircularProgress extends React.PureComponent {
     }
   }
 
-  animate(toVal, dur, ease) {
+  animate(toVal, dur, ease, onComplete) {
     const toValue = toVal || this.props.fill;
     const duration = dur || this.props.duration;
     const easing = ease || this.props.easing;
+    let animatedValue = 0;
+
+    if (onComplete) {
+      this.state.fillAnimation.addListener((value) => {
+        animatedValue = value.value;
+      });
+    }
 
     return Animated.timing(this.state.fillAnimation, {
       toValue,
       easing,
       duration,
-    }).start(this.props.onAnimationComplete);
+    }).start(() => {
+      this.state.fillAnimation.removeAllListeners();
+      if(onComplete) {
+        // Whenever animation completes, provide the last animated value with it.
+        // This value might not be equal to "toValue" if this animation was interrupted(cancel/reset).
+        onComplete(animatedValue);
+      } else if (this.props.onAnimationComplete) {
+        this.props.onAnimationComplete();
+      }
+    });
   }
 
   render() {
